055c3b1ddf59c9c4244f784257b9c8c7c1d82a75
licette32 <beverly.jlgc@gmail.com>
2025-09-11 23:32:05 -0300

    feat: Estructura de carpetas añadida

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name= 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Beverly Gonzalez',
+    author_email= 'beverly.jlgc@gmail.com',
+    packages= find_packages(),
+    install_requires= [],
+)
